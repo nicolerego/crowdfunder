01ca7c3ef19d2caa5e7889ac38330de7ee9f7ade
JRumble <jody.paget@rogers.com>
2015-03-04 18:48:41 -0500

    wip for the users profile/show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ActiveRecord::Base
 	belongs_to :owner, class_name: 'User'
 	has_many :rewards
 
-	validates :owner, presences: true
+	validates :owner, presence: true
 	# has_many :pledges, through: :rewards
 
 	accepts_nested_attributes_for :rewards, reject_if: :all_blank, allow_destroy: :true
