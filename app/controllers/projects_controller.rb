0dc418a704c087d14755a08936c093956d0a3a87
Rolande Saliba <rolande.saliba@gmail.com>
2015-03-04 21:59:49 -0500

    Added the show project, the edit links and page, done some fixes, and added some links throughout the app, and  working on the nested reward form and asociation issues

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,9 @@ class ProjectsController < ApplicationController
 	end
 
 	def create
-		@project = Project.new(project_params)
+
+		@project = current_user.owned_projects.new(project_params)
+		# @project = Project.new(project_params)
 		if @project.save
 			redirect_to project_url(@project), notice: "Project was created!"
 		else
@@ -27,7 +29,7 @@ class ProjectsController < ApplicationController
   	end
 
 	def update
-		@project = Project.find(parmas[:id])
+		@project = Project.find(params[:id])
 		if @project.update_attributes(project_params)
 			redirect_to projects_path(@project)
 		else
